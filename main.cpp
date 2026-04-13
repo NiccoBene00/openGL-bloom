7243361c1bfa9ecf0b71f026f716f22fb1a542e1
Niccolò <niccolo.benedetto@edu.unifi.it>
2026-04-13 21:31:06 +0200

    added lighting system to the rotated 3D cube

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "src/shader.h"
 
 // dimensioni finestra
@@ -21,6 +25,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_DEPTH_BITS, 24);
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Bloom Project", NULL, NULL);
     if (window == NULL)
@@ -40,53 +45,55 @@ int main()
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     // ---------------- SHADER ----------------
     Shader shader("shaders/basic.vert", "shaders/basic.frag");
 
     // ---------------- VERTEX DATA ----------------
     float vertices[] = {
-        // cubo (solo posizioni)
-        -0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
+        // positions        // normals
+        -0.5f,-0.5f,-0.5f,  0.0f,0.0f,-1.0f,
+        0.5f,-0.5f,-0.5f,  0.0f,0.0f,-1.0f,
+        0.5f, 0.5f,-0.5f,  0.0f,0.0f,-1.0f,
+        0.5f, 0.5f,-0.5f,  0.0f,0.0f,-1.0f,
+        -0.5f, 0.5f,-0.5f,  0.0f,0.0f,-1.0f,
+        -0.5f,-0.5f,-0.5f,  0.0f,0.0f,-1.0f,
 
-        -0.5f, -0.5f,  0.5f,
-         0.5f, -0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
+        -0.5f,-0.5f, 0.5f,  0.0f,0.0f,1.0f,
+        0.5f,-0.5f, 0.5f,  0.0f,0.0f,1.0f,
+        0.5f, 0.5f, 0.5f,  0.0f,0.0f,1.0f,
+        0.5f, 0.5f, 0.5f,  0.0f,0.0f,1.0f,
+        -0.5f, 0.5f, 0.5f,  0.0f,0.0f,1.0f,
+        -0.5f,-0.5f, 0.5f,  0.0f,0.0f,1.0f,
 
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
+        -0.5f, 0.5f, 0.5f, -1.0f,0.0f,0.0f,
+        -0.5f, 0.5f,-0.5f, -1.0f,0.0f,0.0f,
+        -0.5f,-0.5f,-0.5f, -1.0f,0.0f,0.0f,
+        -0.5f,-0.5f,-0.5f, -1.0f,0.0f,0.0f,
+        -0.5f,-0.5f, 0.5f, -1.0f,0.0f,0.0f,
+        -0.5f, 0.5f, 0.5f, -1.0f,0.0f,0.0f,
 
-         0.5f,  0.5f,  0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
+        0.5f, 0.5f, 0.5f,  1.0f,0.0f,0.0f,
+        0.5f, 0.5f,-0.5f,  1.0f,0.0f,0.0f,
+        0.5f,-0.5f,-0.5f,  1.0f,0.0f,0.0f,
+        0.5f,-0.5f,-0.5f,  1.0f,0.0f,0.0f,
+        0.5f,-0.5f, 0.5f,  1.0f,0.0f,0.0f,
+        0.5f, 0.5f, 0.5f,  1.0f,0.0f,0.0f,
 
-        -0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f, -0.5f,
-         0.5f, -0.5f,  0.5f,
-         0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f,  0.5f,
-        -0.5f, -0.5f, -0.5f,
+        -0.5f,-0.5f,-0.5f,  0.0f,-1.0f,0.0f,
+        0.5f,-0.5f,-0.5f,  0.0f,-1.0f,0.0f,
+        0.5f,-0.5f, 0.5f,  0.0f,-1.0f,0.0f,
+        0.5f,-0.5f, 0.5f,  0.0f,-1.0f,0.0f,
+        -0.5f,-0.5f, 0.5f,  0.0f,-1.0f,0.0f,
+        -0.5f,-0.5f,-0.5f,  0.0f,-1.0f,0.0f,
 
-        -0.5f,  0.5f, -0.5f,
-         0.5f,  0.5f, -0.5f,
-         0.5f,  0.5f,  0.5f,
-         0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f,  0.5f,
-        -0.5f,  0.5f, -0.5f
+        -0.5f, 0.5f,-0.5f,  0.0f,1.0f,0.0f,
+        0.5f, 0.5f,-0.5f,  0.0f,1.0f,0.0f,
+        0.5f, 0.5f, 0.5f,  0.0f,1.0f,0.0f,
+        0.5f, 0.5f, 0.5f,  0.0f,1.0f,0.0f,
+        -0.5f, 0.5f, 0.5f,  0.0f,1.0f,0.0f,
+        -0.5f, 0.5f,-0.5f,  0.0f,1.0f,0.0f
     };
 
     unsigned int VAO, VBO;
@@ -98,16 +105,43 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // ---------------- RENDER LOOP ----------------
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shader.use();
+        
+        glUniform3f(glGetUniformLocation(shader.ID, "objectColor"), 1.0f, 0.5f, 0.2f);
+        glUniform3f(glGetUniformLocation(shader.ID, "lightColor"), 1.0f, 1.0f, 1.0f);
+        glUniform3f(glGetUniformLocation(shader.ID, "lightPos"), 1.2f, 1.0f, 2.0f);
+
+        // MODEL
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 0.0f));
+
+        // VIEW (camera indietro)
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+
+        // PROJECTION (prospettiva)
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 
+            (float)SCR_WIDTH / (float)SCR_HEIGHT, 
+            0.1f, 
+            100.0f);
+
+        // manda agli shader
+        shader.setMat4("model", glm::value_ptr(model));
+        shader.setMat4("view", glm::value_ptr(view));
+        shader.setMat4("projection", glm::value_ptr(projection));
+
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
--- src/shader.h
+++ src/shader.h
@@ -14,6 +14,8 @@ public:
 
     void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
+
+    void setMat4(const std::string &name, const float* value) const;
 };
 
 #endif
